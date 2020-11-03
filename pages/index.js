515b22d496209357294151bed4a51bb2328a2fa6
Rubén Guerrero <yo@rubenguerrero.com>
2020-11-03 17:35:08 +0100

    feat: add blog page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -15,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -48,6 +50,10 @@ export default function Home() {
             </p>
           </a>
         </div>
+
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +62,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
